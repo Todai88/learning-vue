08c2f2c98dfdc7cbd730e855b234d61e67e1010d
Todai88 <joabaj88@gmail.com>
2020-05-12 11:55:03 +0100

    Lesson 5: add dynamic routing

--- real-world-vue/src/views/EventShow.vue
+++ real-world-vue/src/views/EventShow.vue
@@ -1,7 +1,13 @@
 <template>
-  <h1>Showing event #1</h1>
+  <h1>Showing event #{{ id }}</h1>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    id: {
+      type: String
+    }
+  }
+}
 </script>
