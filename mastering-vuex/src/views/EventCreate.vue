f5572f744c1777709fd91428498676c971bf3123
Todai88 <joabaj88@gmail.com>
2020-05-15 16:03:58 +0100

    Lesson 1: implement store

--- mastering-vuex/src/views/EventCreate.vue
+++ mastering-vuex/src/views/EventCreate.vue
@@ -1,7 +1,21 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create and Event, {{ user.name }}</h1>
+    <p>Event created by {{ user.name }}</p>
+    <p>There are {{ categoriesLength }} categories:</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>{{ getEventById(1) }}</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['categoriesLength', 'getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
 </script>
